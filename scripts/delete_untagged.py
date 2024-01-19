55c0c7072b428dfdb64c0b917bb0ef5bcbdb22a3
Mathias Schreiner <matschreiner@gmail.com>
2024-01-19 13:46:59 +0100

    pylint ignore

--- scripts/delete_untagged.py
+++ scripts/delete_untagged.py
@@ -5,19 +5,11 @@ import wandb
 
 def main(args):
 
-    """
-    deletes all models that do not have a tag attached
-
-    by default this means wandb will delete all but the "latest" or "best" models
-
-    set dry_run == False to delete...
-    """
 
     api = wandb.Api(overrides={"project": args.project})
     project = api.project(args.project)
 
-    for artifact_type in project.artifacts_types():
-        collections = artifact_type.collections()
+    for artifact_type in project.artifacts_types():  # pylint: disable=too-many-nested-blocks
         for artifact_collection in artifact_type.collections():
             artifacts = api.artifacts(artifact_type.type, artifact_collection.name)
             for artifact in artifacts:
@@ -34,8 +26,5 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("project")
     parser.add_argument("--dry_run", action="store_true")
-
-    # parser.add_argument('arg')
-    # parser.add_argument('--kwarg')
 
     main(parser.parse_args())
