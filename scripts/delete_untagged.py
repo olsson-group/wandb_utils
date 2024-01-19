5411db1e127c20eb60c343fe057145d100f17d11
Mathias Schreiner <matschreiner@gmail.com>
2024-01-19 15:24:11 +0100

    delete in parallel

--- scripts/delete_untagged.py
+++ scripts/delete_untagged.py
@@ -1,23 +1,42 @@
 from argparse import ArgumentParser
 
 import wandb
+import concurrent
 
 
 def main(args):
+
     api = wandb.Api(overrides={"project": args.project})
     project = api.project(args.project)
+
+    tasks = []
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=20)
 
     for artifact_type in project.artifacts_types():  # pylint: disable=too-many-nested-blocks
         for artifact_collection in artifact_type.collections():
             artifacts = api.artifacts(artifact_type.type, artifact_collection.name)
             for artifact in artifacts:
                 if artifact_type.type == "model":
-                    if len(artifact.aliases) > 0:
-                        print(f"KEEPING {artifact.name}")
-                    else:
-                        print(f"DELETING {artifact.name}")
-                        if not args.dry_run:
-                            artifact.delete()
+                    executor.submit(delete, artifact, args.dry_run)
+
+
+    executor.shutdown(wait=True)
+
+    
+
+
+                            
+
+
+
+
+def delete(artifact, dry_run):
+    if len(artifact.aliases) > 0:
+        print(f"KEEPING {artifact.name}")
+    else:
+        print(f"DELETING {artifact.name}")
+        if not dry_run:
+            artifact.delete()
 
 
 if __name__ == "__main__":
